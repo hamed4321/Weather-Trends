22c0ee30168a4400b8fd4dbb54ba0de387a6f82c
Hamed <waveyhamz@gmail.com>
2018-06-14 16:49:11 +0100

    Add secure connection.

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,6 @@
 import axios from 'axios'
 const API_KEY = '9b88933608030390ee663ec2cd3cfc68';
-const ROOT_URL = `http://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
+const ROOT_URL = `https://api.openweathermap.org/data/2.5/forecast?appid=${API_KEY}`;
 export const FETCH_WEATHER = 'FETCH_WEATHER';
 
 export function fetchWeather(city){
